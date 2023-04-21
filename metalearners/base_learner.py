241222a359ce45b9b7d097d54f4669d4ff380dcf
leightonvg <leightonvangellecom@gmail.com>
2023-04-21 17:02:33 +0200

    add add_configuration()

--- metalearners/base_learner.py
+++ metalearners/base_learner.py
@@ -8,7 +8,8 @@ from metadatabase import MetaDataBase
 
 class BaseLearner:
     def __init__(self):
-        self._top_configurations: Optional[List[Pipeline]] = None  # if applicable: high-to-low ranked on expected performance
+        self._top_configurations: List[Pipeline] = []  # ranked on expected performance
+        self._configuration_scores: List[float] = []  # scores to rank configurations by
 
     def offline_phase(self, mdbase: MetaDataBase, **kwargs) -> None:
         """Performs offline computation for the `online_phase()` using the specified metadatabase.
@@ -51,10 +52,7 @@ class BaseLearner:
 
     def get_number_of_configurations(self) -> int:
         "Returns an integer indicating the number of stored solutions, 0 if none are stored."
-        if self._top_configurations is None:
-            return 0
-        else:
-            return len(self._top_configurations)
+        return len(self._top_configurations)
 
     def get_top_configurations(self, n: int) -> List[Pipeline]:
         """Get the top solutions stored curing the `online_phase`
@@ -65,6 +63,41 @@ class BaseLearner:
             the amount of solutions that should be returned
 
         """
-        if self._top_configurations is None:
+        if len(self._top_configurations) == 0:
             raise Warning("Meta-Learner has no configurations to return")
         return self._top_configurations[:n]
+
+    def add_configuration(self, configuration: Pipeline, score: float, higher_is_better: bool = True) -> None:
+        """Adds configuration `configuration` to self._top_configurations in approriate place, according to `higher_is_better`
+
+        configuration: Pipeline:
+            The pipeline configuration to add
+        score: float,
+            the score the `configuration` is estimated to have, to rank it by using `higher_is_better` in `self.top_configurations`
+        higher_is_better: boolean
+            True:  a higher `score` is better, False: a lower score is better
+        """
+        # simply add if first entry
+        if len(self._configuration_scores) == 0 and len(self._top_configurations) == 0:
+            self._top_configurations = [configuration]
+            self._configuration_scores = [score]
+            return
+
+        added = False
+        for i, config_score in enumerate(self._configuration_scores):
+            if higher_is_better:
+                if score > config_score:
+                    self._top_configurations.insert(i, configuration)
+                    self._configuration_scores.insert(i, score)
+                    added = True
+                    break
+            else:  # lower is better
+                if score < config_score:
+                    self._top_configurations.insert(i, configuration)
+                    self._configuration_scores.insert(i, score)
+                    added = True
+                    break
+        if not added:  # add at end if necessary
+            new_index = self.get_number_of_configurations() + 1
+            self._top_configurations.insert(new_index, configuration)
+            self._configuration_scores.insert(new_index, score)
