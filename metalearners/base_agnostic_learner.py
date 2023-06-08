6f9b7ab705a8ee138b8c4deca59ab2ec9fe2d963
leightonvg <leightonvangellecom@gmail.com>
2023-06-08 15:34:11 +0200

    fix BaseAgnosticLearner documentation and init

--- metalearners/base_agnostic_learner.py
+++ metalearners/base_agnostic_learner.py
@@ -1,17 +1,24 @@
+import pandas as pd
+
 from metalearners.base_learner import BaseLearner
 
 
 class BaseAgnosticLearner(BaseLearner):
     def __init__(self):
-        pass
+        super().__init__()
 
-    def online_phase(self, max_time: int, metric: str, n_jobs: int) -> None:
+    def online_phase(self, X: pd.DataFrame, y: pd.DataFrame, max_time: int, metric: str, n_jobs: int, total_n_configs: int) -> None:
         """Execute the meta-learning strategy, with the previous `offline_phase` knowledge,
-        but without specification of dataset, within the specified time limit (`max_time`) in seconds.
-        Should at least store the following: best solutions (sklearn.pipeline.Pipline) in self._top_configurations
+        on the specified dataset (`X, `y``) within the specified time limit (`max_time`) in seconds.
+        Should at least store the following: best `n_configs` solutions (sklearn.pipeline.Pipline) in self._top_configurations
+        Note: avoid passing a dataset (`X`,`y`) which is also in `offline_phase()`'s metadatabase `mdbase`
 
         Arguments
         ---------
+        X: pd.DataFrame,
+            Features that are used during pipeline training and possible characterization and similarity methods.
+        y: pd.Series,
+            Targets that are used during pipeline training and possible characterization and similarity methods.
         max_time: int,
             The amount of time the online phase is allowed to take. Additionally, when evaluating the method,
                 the evaluation method such as LOOCV should take care of time keeping as well.
@@ -21,5 +28,8 @@ class BaseAgnosticLearner(BaseLearner):
         n_jobs: int,
             the `n_jobs` the online phase can use in its computations, especially important for meta-learners
             that evaluate models because they could evaluate a lot more or less depending on this value.
+        total_n_configs: integer,
+            specifies the number of configurations that should be stored in self._top_configurations
+            (ordered high-to-low by estimated performance)
         """
         raise NotImplementedError("Must be implemented by child class.")
